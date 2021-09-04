08ccfaa16a3c0f81c0ff065df13b4f07c9662c31
Yusuf <y.abdulla37@gmail.com>
2021-09-04 14:20:52 -0400

    combined states into one object

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,106 +1,109 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 
 import "components/Application.scss";
-import { useState } from 'react'; 
+import { useState } from "react";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
 
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-      
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
- {
-  id: 5,
-  time: "4pm",
-  interview: {
-    student: "Lydia Miller-Jones",
-    interviewer: {
-      id: 1,
-      name: "Sylvia Palmer",
-      avatar: "https://i.imgur.com/LpaY82x.png",
-    }
-  }
-}
-
-];
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+// ];
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  
-  useEffect ( ()=> {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const dailyAppointments = [];
+
+  const setDay = (day) => setState({ ...state, day });
+  // const setDays = (days) => setState((prev) => ({ ...prev, days }));
+
+  useEffect(() => {
     const testURL = `http://localhost:8001/api/days`;
-    axios.get(testURL).then(response => {
-      console.log(response.data);
+    axios.get(testURL).then((response) => {
       setDays([...response.data]);
-     
-    }, []);
-  
-  })
-  
- 
+    });
+  }, []);
+console.log(state);
+
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList days={days} day={days} setDays={setDays} />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={state.days} day={state.day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />
-
-        )}
+        {dailyAppointments.map((appointment) => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
